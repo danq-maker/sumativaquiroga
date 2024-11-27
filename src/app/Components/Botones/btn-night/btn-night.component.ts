5da491e410da1480891f38edcb0240a5956571c8
dquirogagonzalez08 <dquirogagonzalez08@gmail.com>
2024-11-27 04:39:11 +0000

    First commit

--- src/app/Components/Botones/btn-night/btn-night.component.ts
+++ src/app/Components/Botones/btn-night/btn-night.component.ts
@@ -3,16 +3,23 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-btn-night',
   standalone: true,
-  imports: [],
   templateUrl: './btn-night.component.html',
-  styleUrl: './btn-night.component.css'
+  styleUrls: ['./btn-night.component.css']
 })
 export class BtnNightComponent {
-  @Output() toggleDarkMode  = new EventEmitter<void>(); // Emite un evento al hacer clic
+  @Output() toggleDarkMode = new EventEmitter<void>(); // Emite un evento al hacer clic
   isDarkMode: boolean = false; // Variable que indica si el modo oscuro está activado
 
   onClick(): void {
     this.isDarkMode = !this.isDarkMode; // Cambia el estado de isDarkMode a su valor opuesto
-    this.toggleDarkMode .emit(); // Emitir el evento al hacer clic
+    this.toggleDarkMode.emit(); // Emitir el evento al hacer clic
+  }
+
+  get buttonText(): string {
+    return this.isDarkMode ? 'Modo noche' : 'Modo diurno'; // Cambia el texto del botón según el estado
+  }
+
+  get buttonBackgroundColor(): string {
+    return this.isDarkMode ? '#707070' : '#ff5733'; // Cambia el color de fondo según el modo
   }
 }
